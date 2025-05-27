f5182e662e46f71f338272af95ba7fa1937a7093
Swatiyadavv <swatiyadav66145@gmail.com>
2025-05-27 15:44:42 +0530

    remove enc file

--- utils/helper.js
+++ utils/helper.js
@@ -1,34 +1,4 @@
-// function otp(n) {
-//     let otp = "";
-//     for (let index = 0; index < n; index++) {
-//       otp += Math.floor(Math.random() * 10);
-//     }
-//     return otp;
-//   }
-  
-//   const nodemailer = require("nodemailer");
-//   const transporter = nodemailer.createTransport({
-//     host: "smtp.gmail.com",
-//     port: 587,
-//     auth: {
-//       user: "swatiyadav66145@gmail.com",
-//       pass: "cwaz mvow jrov rkiy",
-//     },
-//   });
-//   const sentOtp = async (email, otp) => {
-//     try {
-//       const data = await transporter.sendMail({
-//         from: "<swatiyadav66145@gmail.com>",
-//         to: email,
-//         subject: "OTP",
-//         text: "otp verification",
-//         html: `<b>your OTP is <br/> <h1>${otp}</h1> <b>`,
-//       });
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   };
-// module.exports = { otp, sentOtp };
+
   require("dotenv").config();
 const nodemailer = require("nodemailer");
 
